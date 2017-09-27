327ea16a500d17f1f6935aa6b06d127c37ad57bb
Rodolfo Puig <rodolfo@puig.io>
2017-09-27 15:04:31 -0400

    Ensuring all dependencies are installed

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ This repository setups an environment to my liking.
 
 ## Usage
 
+Switch to ZSH shell:
+
+    chsh -s zsh
+
 Using git:
 
     git clone https://github.com/rudisimo/dotfiles2.git ~/.dotfiles && ~/.dotfiles/install.sh
--- install.sh
+++ install.sh
@@ -10,10 +10,12 @@ cp $BASEDIR/dist/gitconfig $HOME/.gitconfig
 cp $BASEDIR/dist/zshrc $HOME/.zshrc
 cp $BASEDIR/dist/rudisimo.zsh-theme $HOME/.oh-my-zsh/themes/rudisimo.zsh-theme
 
-# configure vim
-[ ! -e "$HOME/.vim/bundle/Vundle.vim" ] && git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
-cp $BASEDIR/dist/vimrc $HOME/.vimrc
-
 # configure tmux
 [ ! -e "$HOME/.tmux/plugins/tpm" ] && git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
 cp $BASEDIR/dist/tmux.conf $HOME/.tmux.conf
+$HOME/.tmux/plugins/tpm/scripts/install_plugins.sh
+
+# configure vim
+[ ! -e "$HOME/.vim/bundle/Vundle.vim" ] && git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+cp $BASEDIR/dist/vimrc $HOME/.vimrc
+vim +BundleInstall +BundleClean +qall
