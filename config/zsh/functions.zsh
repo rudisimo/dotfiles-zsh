97b46c7c30758b8bd9c495bc2d932a8780db92f7
Rodolfo Puig <rodolfo@puig.io>
2018-04-20 12:26:18 -0400

    Updating configuration files

--- config/zsh/functions.zsh
+++ config/zsh/functions.zsh
@@ -50,27 +50,44 @@ function codepoint() {
 
 # Fetch remote file and cat gzip output
 function zurl() {
-  curl "$@" | zcat
+    curl "$@" | zcat
 }
 
 # Extract most know archives with one command
 function extract() {
-  if [ -f $1 ] ; then
-    case $1 in
-      *.tar.bz2)   tar xjf $1     ;;
-      *.tar.gz)    tar xzf $1     ;;
-      *.bz2)       bunzip2 $1     ;;
-      *.rar)       unrar e $1     ;;
-      *.gz)        gunzip $1      ;;
-      *.tar)       tar xf $1      ;;
-      *.tbz2)      tar xjf $1     ;;
-      *.tgz)       tar xzf $1     ;;
-      *.zip)       unzip $1       ;;
-      *.Z)         uncompress $1  ;;
-      *.7z)        7z x $1        ;;
-      *)     echo "'$1' cannot be extracted via extract()" ;;
-    esac
-  else
-    echo "'$1' is not a valid file"
-  fi
+    if [ -f $1 ]; then
+        case $1 in
+        *.tar.bz2)   tar xjf $1     ;;
+        *.tar.gz)    tar xzf $1     ;;
+        *.bz2)       bunzip2 $1     ;;
+        *.rar)       unrar e $1     ;;
+        *.gz)        gunzip $1      ;;
+        *.tar)       tar xf $1      ;;
+        *.tbz2)      tar xjf $1     ;;
+        *.tgz)       tar xzf $1     ;;
+        *.zip)       unzip $1       ;;
+        *.Z)         uncompress $1  ;;
+        *.7z)        7z x $1        ;;
+        *)     echo "'$1' cannot be extracted via extract()" ;;
+        esac
+    else
+        echo "'$1' is not a valid file"
+    fi
+}
+
+# Convert JSON to YAML
+function yq() {
+    python -c 'import sys, yaml, json; yaml.safe_dump(json.load(sys.stdin), sys.stdout, default_flow_style=False)'
+}
+
+# Find a file inverse recursive
+function upsearch() {
+    if [ -n $1 ]; then
+        slashes=${PWD//[^\/]/}
+        directory="$PWD"
+        for (( n=${#slashes}; n>0; --n )); do
+            test -f "${directory}/${1}" && echo "${directory}/${1}" && return
+            directory="${directory}/.."
+        done
+    fi
 }
