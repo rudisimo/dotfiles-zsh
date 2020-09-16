546ea29562031ef951a380d0d9d0dff9488a1488
Rodolfo Puig <rodolfo@puig.io>
2020-09-16 18:54:05 -0400

    Updating installation script

--- install.sh
+++ install.sh
@@ -1,126 +1,192 @@
-#!/bin/bash -eu
+#!/bin/bash
 
-# Define base directory
+set -e
+set -u
+
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# Checks if the given command is available
 is_installed() {
     command -v "$1" >/dev/null 2>&1
 }
 
-# Checks if the target can be configured
 is_configurable() {
     if is_installed "$1"; then
         echo "1"
     fi
 }
 
-# Downloads the given repository from github only once
-download_once() {
-    local repository="$1"
-    local destination="$2"
-    local git_clone="${3-}"
+# Define runtime variables
+has_git=$(is_configurable git)
+has_zsh=$(is_configurable zsh)
+has_vim=$(is_configurable vim)
+has_tmux=$(is_configurable tmux)
 
-    if [ ! -d "$destination" ]; then
-        if [ -n "$git_clone" ]; then
-            git clone -q "https://github.com/${repository}.git" "$destination"
-        else
-            mkdir -p $destination
-            curl -#skfL "https://github.com/${repository}/tarball/master" | tar xzv --strip-components 1 -C $destination
+# Parse command-line arguments
+with_force=
+with_dryrun=
+setup_nodenv=
+setup_pyenv=
+setup_goenv=
+setup_wsl=
+
+OPTIND=1
+while getopts "hfdnpgw" opt; do
+    case "$opt" in
+        f ) with_force=1   ;;
+        d ) with_dryrun=1  ;;
+        n ) setup_nodenv=1 ;;
+        p ) setup_pyenv=1  ;;
+        g ) setup_goenv=1  ;;
+        w ) setup_wsl=1    ;;
+
+        h|\?) cat <<EOF
+Usage: $(basename $0)
+  General:
+    -f    Ignore warning message
+    -d    Dry-run only
+
+  Features:
+    -n    Setup nodenv
+    -p    Setup pyenv
+    -g    Setup goenv
+    -w    Setup WSL
+EOF
+        exit 0
+        ;;
+    esac
+done
+shift $(($OPTIND-1))
+
+# Downloads a Github repository only once
+download_once() {
+    local src="$1"
+    local dst="$2"
+
+    if [ -z "$with_dryrun" ]
+    then
+        if [ ! -d "$dst" ]
+        then
+            if [ -n "$has_git" ]
+            then
+                git clone -q --depth 1 "https://github.com/${src}.git" $dst
+            else
+                mkdir -p $dst
+                curl -#skfL "https://github.com/${src}/tarball/master" | tar xzv --strip-components 1 -C $dst
+            fi
         fi
     fi
 }
 
-# Check hard requirements
-if ! is_installed "git"; then
-    (>&2 echo "Git does not appear to be installed and it is a requirement.")
-    exit 127
+# Copy a file
+copy_file() {
+    local src="$1"
+    local dst="$2"
+
+    if [ -z "$with_dryrun" ]
+    then
+        if [ -e "$src" ]
+        then
+            cp "$src" "$dst"
+        fi
+    fi
+}
+
+# confirm configuration consent
+if [ -z "${with_dryrun}" ] && [ -z "${with_force}" ]
+then
+   read -p "This may overwrite existing files. Are you sure? (y/n) " -n 1;
+   echo "";
+   if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+        echo "[INFO] Aborting..." >&2
+        exit 0
+   fi
 fi
 
-# Parse command-line arguments
-OPTIND=1
-use_clone=1
-use_force=
-configure_git=
-configure_zsh=
-configure_vim=
-configure_tmux=
-while getopts "dfgzvma" opt; do
-case "$opt" in
-    d ) use_clone=;;
-    f ) use_force=1;;
-    b ) use_backup=1;;
+# configure git
+if [ -n "$has_git" ]
+then
+    echo "[INFO] Configuring git..."
 
-    g ) configure_git=$(is_configurable "git");;
-    z ) configure_zsh=$(is_configurable "zsh");;
-    v ) configure_vim=$(is_configurable "vim");;
-    m ) configure_tmux=$(is_configurable "tmux");;
-
-    a )
-        configure_git=$(is_configurable "git")
-        configure_zsh=$(is_configurable "zsh")
-        configure_vim=$(is_configurable "vim")
-        configure_tmux=$(is_configurable "tmux")
-        ;;
-    \?) echo "Usage: $(basename $0) [-c] [-g] [-z] [-v] [-m] [-a]";;
-  esac
-done
-shift $(($OPTIND-1))
-
-# Confirm configuration consent
-#if [ -z "${use_force}" ]; then
-#    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-#    echo "";
-#    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-#        exit 0
-#    fi
-#fi
-
-# Configure git
-if [ -n "$configure_git" ]; then
-    echo "==> Configuring git..."
-    # install global configuration
-    cp $BASEDIR/config/git/.gitconfig $HOME/.gitconfig
+    # Install global configuration
+    copy_file "git/.gitconfig" $HOME/.gitconfig
 fi
 
-# Configure zsh
-if [ -n "$configure_zsh" ]; then
-    echo "==> Configuring zsh..."
+# configure zsh
+if [ -n "$has_zsh" ]; then
+    echo "[INFO] Configuring zsh..." >&2
+
     # warn about shell usage
     if [[ ! $SHELL =~ "zsh" ]]; then
-        (>&2 echo "==> ZSH is not configured as your default shell. This configuration will not work until you switch to it (chsh -s `which zsh`)")
+        echo "[ERRO] ZSH is not configured as your default shell" >&2
+        echo "[INFO] This configuration will not work until you switch to it" >&2
     fi
-    # download oh-my-zsh
-    download_once "robbyrussell/oh-my-zsh" "$HOME/.oh-my-zsh" $use_clone
+
+    # download dependencies
+    download_once robbyrussell/oh-my-zsh $HOME/.oh-my-zsh
+
     # install global configuration
-    cp $BASEDIR/config/zsh/.zshrc $HOME/.zshrc
-    # install custom theme
-    download_once "romkatv/powerlevel10k" "$HOME/.oh-my-zsh/custom/themes/powerlevel9k" $use_clone
-    # download_once "gangleri/pipenv" "$HOME/.oh-my-zsh/custom/plugins/pipenv" $use_clone
+    copy_file $BASEDIR/config/zsh/.zshrc $HOME/.zshrc
+
     # install custom scripts
-    cp $BASEDIR/config/zsh/aliases.zsh $HOME/.oh-my-zsh/custom/
-    cp $BASEDIR/config/zsh/functions.zsh $HOME/.oh-my-zsh/custom/
-    cp $BASEDIR/config/zsh/man.zsh $HOME/.oh-my-zsh/custom/
-    cp $BASEDIR/config/zsh/powerlevel9k.zsh $HOME/.oh-my-zsh/custom/
+    copy_file $BASEDIR/config/zsh/aliases.zsh $HOME/.oh-my-zsh/custom
+    copy_file $BASEDIR/config/zsh/functions.zsh $HOME/.oh-my-zsh/custom
+    copy_file $BASEDIR/config/zsh/man.zsh $HOME/.oh-my-zsh/custom
+
+    # install p10k theme
+    download_once romkatv/powerlevel10k $HOME/.oh-my-zsh/custom/themes/powerlevel10k
+    copy_file $BASEDIR/config/zsh/.p10k.zsh $HOME/.p10k.zsh
+
+    # install nodenv
+    if [ -n "$setup_nodenv" ]
+    then
+        echo "[INFO] Configuring nodenv..." >&2
+        download_once nodenv/nodenv $HOME/.nodenv
+        copy_file $BASEDIR/config/zsh/nodenv.zsh $HOME/.oh-my-zsh/custom
+    fi
+
+    # install pyenv
+    if [ -n "$setup_pyenv" ]
+    then
+        echo "[INFO] Configuring pyenv..." >&2
+        download_once pyenv/pyenv $HOME/.pyenv
+        copy_file $BASEDIR/config/zsh/pyenv.zsh $HOME/.oh-my-zsh/custom
+    fi
+
+    # install goenv
+    if [ -n "$setup_goenv" ]
+    then
+        echo "[INFO] Configuring goenv..." >&2
+        download_once syndbg/goenv $HOME/.goenv
+        copy_file $BASEDIR/config/zsh/goenv.zsh $HOME/.oh-my-zsh/custom
+    fi
+
+    # install WSL
+    if [ -n "$setup_wsl" ]
+    then
+        copy_file $BASEDIR/config/zsh/wsl.zsh $HOME/.oh-my-zsh/custom
+    fi
 fi
 
-# Configure vim
-if [ -n "$configure_vim" ]; then
-    echo "==> Configuring vim..."
-    # download vundle
-    download_once "VundleVim/Vundle.vim" "$HOME/.vim/bundle/Vundle.vim" $use_clone
+# configure vim
+if [ -n "$has_vim" ]; then
+    echo "[INFO] Configuring vim..." >&2
+
     # install global configuration
-    cp $BASEDIR/config/vim/.vimrc $HOME/.vimrc
-    # install Vundle plugins
+    copy_file $BASEDIR/config/vim/.vimrc $HOME/.vimrc
+
+    # install vundle
+    download_once VundleVim/Vundle.vim $HOME/.vim/bundle/Vundle.vim
     vim +PluginInstall +PluginClean! +qall --not-a-term -n >/dev/null
 fi
 
-# Configure tmux
-if [ -n "$configure_tmux" ]; then
-    echo "==> Configuring tmux..."
+# configure tmux
+if [ -n "$has_tmux" ]; then
+    echo "[INFO] Configuring tmux..." >&2
+
     # install global configuration
-    cp $BASEDIR/config/tmux/tmux.conf $HOME/.tmux.conf
-    cp $BASEDIR/config/tmux/tmux.local.conf $HOME/.tmux.conf.local
+    copy_file $BASEDIR/config/tmux/tmux.conf $HOME/.tmux.conf
+    copy_file $BASEDIR/config/tmux/tmux.local.conf $HOME/.tmux.conf.local
 fi
 
+echo "[INFO] Done!" >&2
 exit 0
