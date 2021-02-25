b9e0a2268f7f3d2fc9dde9000db3d4c3bd40c7fb
Rodolfo Puig <rodolfo@puig.io>
2021-02-25 09:32:37 -0500

    Updating runtime logic

--- install.sh
+++ install.sh
@@ -28,15 +28,13 @@ EOF
   exit
 }
 
-locate() {
-    command -v $1 2>/dev/null || true
-}
-
 cleanup() {
     trap - SIGINT SIGTERM ERR EXIT
-
-    # remove our staging directory
     test -e "${STAGEDIR}" && rm -rf "${STAGEDIR}"
+}
+
+choose() {
+    command -v $1 2>/dev/null || true
 }
 
 #####################################################
@@ -44,7 +42,7 @@ cleanup() {
 #####################################################
 
 print() {
-    echo >&2 -e "${1-}"
+    [[ ${NO_OUTPUT-} -eq 1 ]] || echo >&2 -e "${1-}"
 }
 
 die() {
@@ -59,9 +57,7 @@ datetime() {
 }
 
 debug() {
-    if [[ ${VERBOSE-} -eq 1 ]]; then
-        print "$(datetime) | ${G}DEBUG${NC} | ${1-}"
-    fi
+    print "$(datetime) | ${G}DEBUG${NC} | ${1-}"
 }
 
 info() {
@@ -85,39 +81,31 @@ fatal() {
 #####################################################
 
 parse_args() {
-    FORCE=0
-    DRYRUN=0
-    VERBOSE=0
-    param=''
-
     while :; do
         case "${1-}" in
-        -h | --help    ) usage ;;
-        -d | --debug   ) set -x ;;
-        -v | --verbose ) VERBOSE=1 ;;
-        --no-color     ) NO_COLOR=1 ;;
-        --dry-run      ) DRYRUN=1 ;;
-        --no-git       ) NO_GIT=1 ;;
-        --no-zsh       ) NO_ZSH=1 ;;
-        --no-vim       ) NO_VIM=1 ;;
-        --no-tmux      ) NO_TMUX=1 ;;
-        -f | --force   ) FORCE=1 ;;
-        -p | --param   ) # example named parameter
-            param="${2-}"
-            shift
-            ;;
-        -?*            ) die "Unknown option: $1" ;;
-        *              ) break ;;
+            --trace        ) set -x ;;
+            -h | --help    ) usage ;;
+            -f | --force   ) FORCE=1 ;;
+            -d | --debug   ) DEBUG=1 ;;
+            -q | --quiet   ) NO_OUTPUT=1 ;;
+            --no-color     ) NO_COLOR=1 ;;
+            --no-git       ) NO_GIT=1 ;;
+            --no-zsh       ) NO_ZSH=1 ;;
+            --no-vim       ) NO_VIM=1 ;;
+            --no-tmux      ) NO_TMUX=1 ;;
+            --dry-run      ) DRYRUN=1 ;;
+            -?*            ) die "Unknown option: $1" ;;
+            *              ) break ;;
         esac
         shift
     done
 
     args=("$@")
 
-    git=$(locate git)
-    zsh=$(locate zsh)
-    vim=$(locate vim)
-    tmux=$(locate tmux)
+    git=$(choose git)
+    zsh=$(choose zsh)
+    vim=$(choose vim)
+    tmux=$(choose tmux)
 
     return 0
 }
@@ -131,59 +119,70 @@ setup_colors() {
 }
 
 #####################################################
-# Installer functions
+# Utility functions
 #####################################################
 
-download_github() {
+download_github_repo() {
     local namespace=$1
     local destination${2-$STAGEDIR/$namespace}
 }
 
+#####################################################
+# Installer functions
+#####################################################
+
+debug_git() {
+    debug "  path: $git"
+    debug "  version: $($git --version | head -n 1)"
+}
+
+debug_zsh() {
+    debug "  path: $zsh"
+    debug "  version: $($zsh --version | head -n 1)"
+}
+
+
+debug_vim() {
+    debug "  path: $vim"
+    debug "  version: $($vim --version | head -n 1)"
+}
+
+
+debug_tmux() {
+    debug "  path: $tmux"
+    debug "  version: $($tmux -V | head -n 1)"
+}
+
+
 configure_git() {
-    if [[ ${NO_GIT-} -eq 0 ]]; then
-        if [ -n "$git" ]; then
-            info "Configuring git"
-            debug "  path: $git"
-            debug "  version: $($git --version)"
-        else
-            warn "git not found, skipping configuration"
-        fi
+    if [[ -n "$git" ]]; then
+        true
+    else
+        warn "git not found, skipping configuration"
     fi
 }
 
 configure_zsh() {
-    if [[ ${NO_ZSH-} -eq 0 ]]; then
-        if [ -n "$zsh" ]; then
-            info "Configuring zsh"
-            debug "  path: $zsh"
-            debug "  version: $($zsh --version)"
-        else
-            warn "zsh not found, skipping configuration"
-        fi
+    if [[ -n "$zsh" ]]; then
+        true
+    else
+        warn "zsh not found, skipping configuration"
     fi
 }
 
 configure_vim() {
-    if [[ ${NO_VIM-} -eq 0 ]]; then
-        if [ -n "$vim" ]; then
-            info "Configuring vim"
-            debug "  path: $vim"
-            debug "  version: $($vim --version | head -n 1)"
-        else
-            warn "vim not found, skipping configuration"
-        fi
+    if [[ -n "$vim" ]]; then
+        true
+    else
+        warn "vim not found, skipping configuration"
     fi
 }
 
 configure_tmux() {
-    if [[ ${NO_TMUX-} -eq 0 ]]; then
-        if [ -n "$tmux" ]; then
-            info "Configuring tmux"
-            debug "  path: $tmux"
-            debug "  version: $($tmux -V)"
-        else
-            warn "tmux not found, skipping configuration"
-        fi
+    if [[ -n "$tmux" ]]; then
+        true
+    else
+        warn "tmux not found, skipping configuration"
     fi
 }
 
@@ -194,10 +193,33 @@ configure_tmux() {
 parse_args "$@"
 setup_colors
 
-configure_git
-configure_zsh
-configure_vim
-configure_tmux
+# configure git, unless disabled
+if [[ ${NO_GIT-} -eq 0 ]]; then
+    info "Configuring git ..."
+    [[ ${DEBUG-} -eq 1 ]] && debug_git
+    configure_git
+fi
+
+# configure zsh, unless disabled
+if [[ ${NO_ZSH-} -eq 0 ]]; then
+    info "Configuring zsh ..."
+    [[ ${DEBUG-} -eq 1 ]] && debug_zsh
+    configure_zsh
+fi
+
+# configure vim, unless disabled
+if [[ ${NO_VIM-} -eq 0 ]]; then
+    info "Configuring vim ..."
+    [[ ${DEBUG-} -eq 1 ]] && debug_vim
+    configure_vim
+fi
+
+# configure tmux, unless disabled
+if [[ ${NO_TMUX-} -eq 0 ]]; then
+    info "Configuring tmux ..."
+    [[ ${DEBUG-} -eq 1 ]] && debug_tmux
+    configure_tmux
+fi
 
 # info  "Read parameters:"
 # debug "- force: ${force}"
