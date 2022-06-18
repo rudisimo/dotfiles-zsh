56fe3034b1a48f401ab6abb4b5f5fbba797f987e
Rodolfo Puig <rodolfo@puig.io>
2022-06-17 23:31:32 -0400

    Github does not like the runner workspace

--- .github/docker/entrypoint-remote.sh
+++ .github/docker/entrypoint-remote.sh
@@ -3,4 +3,4 @@
 set -Eeuo pipefail
 
 echo "Installing ${GITHUB_REPOSITORY-'dotfiles-zsh'} via REMOTE execution (${GITHUB_SHA})"
-curl -s "file://${RUNNER_WORKSPACE}/install.sh" | sh -s -- -p "${RUNNER_TEMP}" -b "${GITHUB_REF_NAME}" -f -v
+curl -s "file://${GITHUB_WORKSPACE}/install.sh" | sh -s -- -p "${RUNNER_TEMP}" -b "${GITHUB_REF_NAME}" -f -v
