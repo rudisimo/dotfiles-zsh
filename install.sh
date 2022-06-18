14ce0180ed361b3aaae1299e78b63994c51441e6
Rodolfo Puig <rodolfo@puig.io>
2022-06-17 21:37:28 -0400

    Updating install script to use POSIX-directives only

--- install.sh
+++ install.sh
@@ -1,224 +1,340 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-set -Eeuo pipefail
-trap cleanup SIGINT SIGTERM ERR EXIT
+set -e -u
+trap cleanup 15 2 0
 
-BASEDIR=
+#####################################################
+# Global variables
+#####################################################
+
 STAGEDIR=$(mktemp -d -q)
-if [ -n "${BASH_SOURCE-}" ]; then
-    BASEDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+DESTDIR=$HOME
+BASEDIR=
+if [ -t 0 ]; then
+    BASEDIR=$(cd -- "$(dirname -- "$0")" && pwd -P)
 fi
+
+# Options
+OPT_USECURL=
+OPT_FORCE=
+OPT_VERBOSE=
+OPT_QUIET=
+OPT_DRYRUN=
+OPT_COLOR=1
+OPT_GIT=1
+OPT_ZSH=1
+OPT_VIM=1
+OPT_TMUX=1
+
+# Colors
+NC=
+R=
+G=
+O=
+B=
+M=
+C=
+Y=
 
 #####################################################
 # Core functions
 #####################################################
 
-usage() {
-    cat <<EOF
-Usage: $(basename "${BASH_SOURCE[0]}") [-h] [-d] [-v] [-f] -p param_value arg1 [arg2...]
-
-Available options:
-
--h, --help      Print this help and exit
--d, --debug     Print script debug info
--v, --verbose   Increase output verbosity
--f, --force     Never prompt
--p, --param     Some param description
-EOF
-  exit
-}
-
 cleanup() {
-    trap - SIGINT SIGTERM ERR EXIT
-    test -e "${STAGEDIR}" && rm -rf "${STAGEDIR}"
+    trap - 15 2 0
+    test -e "$STAGEDIR" && rm -rf "$STAGEDIR"
 }
 
-choose() {
-    command -v $1 2>/dev/null || true
-}
-
-#####################################################
-# Output functions
-#####################################################
-
-print() {
-    [[ ${NO_OUTPUT-} -eq 1 ]] || echo >&2 -e "${1-}"
+log() {
+    [ "$OPT_QUIET" = 1 ] || echo >&2 -e "$@"
 }
 
 die() {
-    local message=$1
-    local statuscode=${2-1}
-    print "$message"
-    exit "$statuscode"
+    [ -z "$@" ] || log "$@"
+    exit 1
 }
 
-datetime() {
-    date +'%Y-%m-%dT%H:%M:%S'
+date_iso8601() {
+    date +'%Y-%m-%dT%H:%M:%S%z'
 }
 
 debug() {
-    print "$(datetime) | ${G}DEBUG${NC} | ${1-}"
+    log "$(date_iso8601) | ${G}DEBUG${NC} | ${1-}"
 }
 
 info() {
-    print "$(datetime) | INFO  | ${1-}"
+    log "$(date_iso8601) | INFO  | ${1-}"
 }
 
 warn() {
-    print "$(datetime) | ${Y}WARN${NC}  | ${1-}"
+    log "$(date_iso8601) | ${Y}WARN${NC}  | ${1-}"
 }
 
 error() {
-    print "$(datetime) | ${R}ERROR${NC} | ${1-}"
+    log "$(date_iso8601) | ${R}ERROR${NC} | ${1-}"
 }
 
 fatal() {
-    die "$(datetime) | ${M}FATAL${NC} | ${1-}"
-}
-
-#####################################################
-# CLI functions
-#####################################################
-
-parse_args() {
-    while :; do
-        case "${1-}" in
-            --trace        ) set -x ;;
-            -h | --help    ) usage ;;
-            -f | --force   ) FORCE=1 ;;
-            -d | --debug   ) DEBUG=1 ;;
-            -q | --quiet   ) NO_OUTPUT=1 ;;
-            --no-color     ) NO_COLOR=1 ;;
-            --no-git       ) NO_GIT=1 ;;
-            --no-zsh       ) NO_ZSH=1 ;;
-            --no-vim       ) NO_VIM=1 ;;
-            --no-tmux      ) NO_TMUX=1 ;;
-            --dry-run      ) DRYRUN=1 ;;
-            -?*            ) die "Unknown option: $1" ;;
-            *              ) break ;;
-        esac
-        shift
-    done
-
-    args=("$@")
-
-    git=$(choose git)
-    zsh=$(choose zsh)
-    vim=$(choose vim)
-    tmux=$(choose tmux)
-
-    return 0
-}
-
-setup_colors() {
-    if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
-        NC='\033[0m' R='\033[0;31m' G='\033[0;32m' O='\033[0;33m' B='\033[0;34m' M='\033[0;35m' C='\033[0;36m' Y='\033[1;33m'
-    else
-        NC='' R='' G='' O='' B='' M='' C='' Y=''
-    fi
+    die "$(date_iso8601) | ${M}FATAL${NC} | ${1-}"
 }
 
 #####################################################
 # Utility functions
 #####################################################
 
-download_github_repo() {
-    local namespace=$1
-    local destination${2-$STAGEDIR/$namespace}
+check_path() {
+    command -v "$1" 2>/dev/null || echo ""
+}
+
+confirm_user_action() {
+    # force user action when requested
+    [ "$OPT_FORCE" = 1 ] && return 0
+
+    # cancel when running in non-iteractive mode
+    [ ! -t 0 ] || [ ! -t 1 ] && fatal "Running in headless-mode. Please pass --force to continue"
+
+    # request user confirmation
+    read -p "${1:-This may overwrite existing files}. Are you sure? (y/n) " -r reply
+    case "$reply" in
+        [yY]* ) return 0;;
+    esac
+
+    return 1
+}
+
+confirm_should_run() {
+    [ ! "$OPT_DRYRUN" = 1 ] && return 0
+    return 1
+}
+
+download() {
+    local repository=$1
+    local repository_url="https://github.com/${repository}"
+    local destination=${2-$STAGEDIR/$repository}
+    local branch=${3-master}
+
+    if confirm_user_action; then
+        info "  download: $repository_url -> $destination"
+        if confirm_should_run; then
+            if [ -z "$OPT_USECURL" ]; then
+                [ -d $destination ] && git -C $destination pull || git clone -q --depth 1 --branch $branch "${repository_url}.git" $destination
+            else
+                mkdir -p $destination
+                curl -sfL "${repository_url}/tarball/${branch}" | tar xzv --strip-components 1 -C $destination
+            fi
+        fi
+    fi
+}
+
+
+download_once() {
+    local repository=$1
+    local destination=${2-$STAGEDIR/$repository}
+
+    if [ ! -e "$destination" ]; then
+        download "$@"
+    fi
+}
+
+copy_file() {
+    local sourcefile=$1
+    local destinationfile=$2
+
+    if [ -e "$sourcefile" ]; then
+        info "  copy: $sourcefile -> $destinationfile"
+        if [ -z "$OPT_DRYRUN" ]; then
+            cp -f $sourcefile $destinationfile
+        fi
+    fi
+}
+
+link_file() {
+    local sourcefile=$1
+    local destinationfile=$2
+
+    if [ -e "$sourcefile" ]; then
+        info "  symlink: $sourcefile -> $destinationfile"
+        if [ -z "$OPT_DRYRUN" ]; then
+            ln -sf $sourcefile $destinationfile
+        fi
+    fi
 }
 
 #####################################################
 # Installer functions
 #####################################################
 
-debug_git() {
-    debug "  path: $git"
-    debug "  version: $($git --version | head -n 1)"
-}
-
-debug_zsh() {
-    debug "  path: $zsh"
-    debug "  version: $($zsh --version | head -n 1)"
-}
-
-
-debug_vim() {
-    debug "  path: $vim"
-    debug "  version: $($vim --version | head -n 1)"
-}
-
-
-debug_tmux() {
-    debug "  path: $tmux"
-    debug "  version: $($tmux -V | head -n 1)"
-}
-
-
 configure_git() {
-    if [[ -n "$git" ]]; then
-        [[ ${DEBUG-} -eq 1 ]] && debug_git
-    else
-        warn "git not found, skipping configuration"
+    if [ "$OPT_GIT" = 1 ]; then
+        info "Configuring git ..."
+
+        git_path=$(check_path git)
+        if [ -n "$git_path" ]; then
+            if [ "$OPT_VERBOSE" = 1 ]; then
+                debug "  path: $git_path"
+                debug "  version: $($git_path --version | head -n 1)"
+            fi
+
+            # install global configuration
+            copy_file $BASEDIR/config/git/.gitconfig $DESTDIR/.gitconfig
+        else
+            warn "git not found, skipping configuration"
+        fi
     fi
 }
 
 configure_zsh() {
-    if [[ -n "$zsh" ]]; then
-        [[ ${DEBUG-} -eq 1 ]] && debug_zsh
-    else
-        warn "zsh not found, skipping configuration"
+    if [ "$OPT_ZSH" = 1 ]; then
+        info "Configuring zsh ..."
+
+        zsh_path=$(check_path zsh)
+        if [ -n "$zsh_path" ]; then
+            if [ "$OPT_VERBOSE" = 1 ]; then
+                debug "  path: $zsh_path"
+                debug "  version: $($zsh_path --version | head -n 1)"
+            fi
+
+            # download oh-my-zsh
+            download robbyrussell/oh-my-zsh $DESTDIR/.oh-my-zsh
+
+            # install global configuration
+            copy_file $BASEDIR/config/zsh/zshrc $DESTDIR/.zshrc
+            copy_file $BASEDIR/config/zsh/zprofile $DESTDIR/.zprofile
+
+            # install custom scripts
+            copy_file $BASEDIR/config/zsh/aliases.zsh $DESTDIR/.oh-my-zsh/custom
+            copy_file $BASEDIR/config/zsh/functions.zsh $DESTDIR/.oh-my-zsh/custom
+            copy_file $BASEDIR/config/zsh/man.zsh $DESTDIR/.oh-my-zsh/custom
+
+            # install custom plugins
+            download zsh-users/zsh-completions $DESTDIR/.oh-my-zsh/custom/plugins/zsh-completions
+            download zsh-users/zsh-autosuggestions $DESTDIR/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+
+            # install p10k theme
+            download romkatv/powerlevel10k $DESTDIR/.oh-my-zsh/custom/themes/powerlevel10k
+            copy_file $BASEDIR/config/zsh/p10k.zsh $DESTDIR/.p10k.zsh
+        else
+            warn "zsh not found, skipping configuration"
+        fi
     fi
 }
 
 configure_vim() {
-    if [[ -n "$vim" ]]; then
-        [[ ${DEBUG-} -eq 1 ]] && debug_vim
-    else
-        warn "vim not found, skipping configuration"
+    if [ "$OPT_VIM" = 1 ]; then
+        info "configuring vim ..."
+
+        vim_path=$(check_path vim)
+        if [ -n "$vim_path" ]; then
+            if [ "$OPT_VERBOSE" = 1 ]; then
+                debug "  path: $vim_path"
+                debug "  version: $($vim_path --version | head -n 1)"
+            fi
+
+            # download vundle
+            download VundleVim/Vundle.vim $DESTDIR/.vim/bundle/Vundle.vim
+
+            # install global configuration
+            copy_file $BASEDIR/config/vim/.vimrc $DESTDIR/.vimrc
+
+            # # install Vundle plugins
+            # vim +PluginInstall +PluginClean! +qall --not-a-term -n >/dev/null
+        else
+            warn "vim not found, skipping configuration"
+        fi
     fi
 }
 
 configure_tmux() {
-    if [[ -n "$tmux" ]]; then
-        [[ ${DEBUG-} -eq 1 ]] && debug_tmux
-    else
-        warn "tmux not found, skipping configuration"
+    if [ "$OPT_TMUX" = 1 ]; then
+        info "configuring tmux ..."
+
+        tmux_path=$(check_path tmux)
+        if [ -n "$tmux_path" ]; then
+            if [ "$OPT_VERBOSE" = 1 ]; then
+                debug "  path: $tmux_path"
+                debug "  version: $($tmux_path -V | head -n 1)"
+            fi
+
+            # download vundle
+            download gpakosz/.tmux $DESTDIR/.tmux
+
+            # install global configuration
+            link_file $DESTDIR/.tmux/.tmux.conf $DESTDIR/.tmux.conf
+            copy_file $BASEDIR/config/tmux/.tmux.conf.local $DESTDIR/.tmux.conf.local
+        else
+            warn "tmux not found, skipping configuration"
+        fi
     fi
+}
+
+#####################################################
+# CLI functions
+#####################################################
+
+usage() {
+    cat <<EOF
+Usage: install.sh [OPTIONS]
+
+Available options:
+  -f, --force       Skip all prompts
+  -q, --quiet       Hide all output
+  -v, --verbose     Increase output verbosity
+EOF
+  exit 1
 }
 
 #####################################################
 # Runtime logic begins here...
 #####################################################
 
-parse_args "$@"
-setup_colors
-
-# configure git, unless disabled
-if [[ ${NO_GIT-} -eq 0 ]]; then
-    info "Configuring git ..."
-    configure_git
+if [ -t 2 ] && [ -n "$OPT_COLOR" ] && [ "${TERM-}" != "dumb" ]; then
+    NC='\033[0m' R='\033[0;31m' G='\033[0;32m' O='\033[0;33m' B='\033[0;34m' M='\033[0;35m' C='\033[0;36m' Y='\033[1;33m'
 fi
 
-# configure zsh, unless disabled
-if [[ ${NO_ZSH-} -eq 0 ]]; then
-    info "Configuring zsh ..."
-    configure_zsh
+while [ "$#" -gt 0 ]; do
+    arg="$1"
+    shift
+    case "$arg" in
+        -h | --help    ) usage ;;
+        -x | --trace   ) set -x ;;
+        -f | --force   ) OPT_FORCE=1 ;;
+        -q | --quiet   ) OPT_QUIET=1 ;;
+        -v | --verbose ) OPT_VERBOSE=1 ;;
+        --dry          ) OPT_DRYRUN=1 ;;
+        --use-curl     ) OPT_USECURL=1 ;;
+        --no-color     ) OPT_COLOR= ;;
+        --no-git       ) OPT_GIT= ;;
+        --no-zsh       ) OPT_ZSH= ;;
+        --no-vim       ) OPT_VIM= ;;
+        --no-tmux      ) OPT_TMUX= ;;
+        -p | --prefix  )
+            DESTDIR="$1"
+            shift
+            ;;
+        -?*            ) die "Unknown option: $arg" ;;
+        *              ) break ;;
+    esac
+done
+
+if [ "$OPT_DRYRUN" = 1 ]; then
+    warn "enabling dry-run mode ..."
 fi
 
-# configure vim, unless disabled
-if [[ ${NO_VIM-} -eq 0 ]]; then
-    info "Configuring vim ..."
-    configure_vim
+# enable cURL downloads
+if [ -z "$(check_path git)" ]; then
+    info "git not installed, using cURL for downloads instead"
+    OPT_USECURL=1
 fi
 
-# configure tmux, unless disabled
-if [[ ${NO_TMUX-} -eq 0 ]]; then
-    info "Configuring tmux ..."
-    configure_tmux
+# download dotfiles when running in headless mode
+if [ -z "$BASEDIR" ]; then
+    info "downloading dotfiles-zsh ..."
+    BASEDIR=$STAGEDIR/dotfiles-zsh
+    download_once rudisimo/dotfiles-zsh $BASEDIR
 fi
 
-# info  "Read parameters:"
-# debug "- force: ${force}"
-# warn  "- verbose: ${verbose}"
-# error "- arguments: ${args[*]-}"
-# fatal "..."
+# configure environment
+configure_git
+configure_zsh
+configure_vim
+configure_tmux
