969d2354887a534b2ca48c5fad207a9c3db8ef86
Rodolfo Puig <rodolfo@puig.io>
2022-06-17 23:02:31 -0400

    Enabling remote tests

--- .github/docker/entrypoint-local.sh
+++ .github/docker/entrypoint-local.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 set -Eeuo pipefail
+
 echo "Installing ${GITHUB_REPOSITORY-'dotfiles-zsh'} via LOCAL execution (${GITHUB_SHA-'HEAD'})"
-sh ./install.sh -v -f
+sh "${GITHUB_WORKSPACE}/install.sh" -v -f
--- .github/docker/entrypoint-remote.sh
+++ .github/docker/entrypoint-remote.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 set -Eeuo pipefail
+
 echo "Installing ${GITHUB_REPOSITORY-'dotfiles-zsh'} via REMOTE execution (${GITHUB_SHA-'HEAD'})"
-curl -s file://$PWD/install.sh | sh -s -- -v -f
+curl -s "file://${GITHUB_WORKSPACE}/install.sh" | sh -s -- -v -f
