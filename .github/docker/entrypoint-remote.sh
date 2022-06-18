29d3a4b43acca4dcd7bfbbdcbfbea41e9a4e0357
Rodolfo Puig <rodolfo@puig.io>
2022-06-17 23:05:08 -0400

    Trying commit SHA during remote test

--- .github/docker/entrypoint-remote.sh
+++ .github/docker/entrypoint-remote.sh
@@ -2,5 +2,5 @@
 
 set -Eeuo pipefail
 
-echo "Installing ${GITHUB_REPOSITORY-'dotfiles-zsh'} via REMOTE execution (${GITHUB_SHA-'HEAD'})"
-curl -s "file://${GITHUB_WORKSPACE}/install.sh" | sh -s -- -v -f
+echo "Installing ${GITHUB_REPOSITORY-'dotfiles-zsh'} via REMOTE execution (${GITHUB_SHA})"
+curl -s "file://${GITHUB_WORKSPACE}/install.sh" | sh -s -- -v -f -b "$GITHUB_SHA"
